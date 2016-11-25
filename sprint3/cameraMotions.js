b281888b3cfa70d83f44873becc1454751d861c6
Ben Myers <ben.myers@okstate.edu>
2016-11-25 01:26:39 -0600

    Added a script to move camera around

---
+++ sprint3/cameraMotions.js
@@ -0,0 +1,27 @@
+function moveCamera(sceneNode)
+{
+    if(pressedKeys[87] == true || pressedKeys[38] == true) // UP
+    {
+        sceneNode.position.y = sceneNode.position.y + frameDuration;
+    }
+    if(pressedKeys[83] == true || pressedKeys[40] == true) // DOWN
+    {
+        sceneNode.position.y = sceneNode.position.y - frameDuration;
+    }
+    if(pressedKeys[65] == true || pressedKeys[37] == true) // LEFT
+    {
+        sceneNode.position.x = sceneNode.position.x - frameDuration;
+    }
+    if(pressedKeys[68] == true || pressedKeys[39] == true) // RIGHT
+    {
+        sceneNode.position.x = sceneNode.position.x + frameDuration;
+    }
+    if(pressedKeys[81] == true || pressedKeys[222] == true) // FORWARD
+    {
+        sceneNode.position.z = sceneNode.position.y + frameDuration;
+    }
+    if(pressedKeys[90] == true || pressedKeys[191] == true) // BACKWARDS
+    {
+        sceneNode.position.z = sceneNode.position.z - frameDuration;
+    }
+}
