5822aa27152e87777ee72be87f361df14ef2f77c
Ben Myers <ben.myers@okstate.edu>
2016-12-07 21:39:11 -0600

    Introduced game states

--- sprintFinal/rockemsockem.js
+++ sprintFinal/rockemsockem.js
@@ -1,4 +1,18 @@
 var fnum = 0;
+var root = currentScene.getObjectByName("rootNode");
+
+// KEYBOARD CONSTANTS
+var Q_KEY = 81;
+var P_KEY = 80;
+var SPACE = 32;
+
+function start()
+{
+    if(root.userData["gameState"] === "waiting" && pressedKeys[SPACE])
+    {
+        root.userData["gameState"] = "playing";
+    }
+}
 
 function cameraTrack(node)
 {
@@ -11,10 +25,13 @@ function cameraTrack(node)
 
 function cameraSpin(node)
 {
-    fnum++;
-    var frameRate = 60.0;
-    var x = 5.0 * Math.cos(fnum/(frameRate*-3) + 4.71239);
-    var z = 5.0 * Math.sin(fnum/(frameRate*-3) + 4.71239);
-    node.position.x = x;
-    node.position.z = z;
+    if(root.userData["gameState"] !== "waiting")
+    {
+        fnum++;
+        var frameRate = 60.0;
+        var x = 5.0 * Math.cos(fnum/(frameRate*-3) + 4.71239);
+        var z = 5.0 * Math.sin(fnum/(frameRate*-3) + 4.71239);
+        node.position.x = x;
+        node.position.z = z;
+    }
 }
